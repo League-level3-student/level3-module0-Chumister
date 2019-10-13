5669addfd28a6f6529a117dfefda91f3a0151632
league <league@imac-9.attlocal.net>
2019-10-13 11:25:52 -0700

    dfdsg

--- src/_01_Simple_Array_Algorithms/_00_1D_Array_Methods.java
+++ src/_01_Simple_Array_Algorithms/_00_1D_Array_Methods.java
@@ -5,14 +5,14 @@ public class _00_1D_Array_Methods {
 	//   of the integers in the array being passed in
 	public static int sumIntArray(int[] values) {
 		
-		return sumIntArray(null);
+		return sumIntArray(values);
 	}
 	
 	//2. Complete the method so that it returns the average of all
 	//   of the integers in the array being passed in
 	public static double averageIntArray(int[] values) {
 		
-		return averageIntArray(null);
+		return averageIntArray(values);
 	}
 	
 	
@@ -20,15 +20,22 @@ public class _00_1D_Array_Methods {
 	//   array contains the value specified by the second parameter.
 	//   It should otherwise return false.
 	public static boolean containsIntValue(int[] array, int value) {
-	
+	if (containsIntValue(array, value)) {
+		return true;
+	}else {
 		return false;
+	}
 	}
 	
 	//4. Complete the method so that it returns the index of the,
 	//   first instance that the specified value occurs in the array.
 	//   If the array does not contain the specified value, it should return -1.
-	public static int getIndex(int[] arr, int value) {
-		
-		return 0;
+	public static boolean getIndex(int[] arr, int value) {
+		if (getIndex(arr,value)) {
+			return getIndex(arr, value);
+		}
+		else {
+			return -1;
+		}
 	}
 }
